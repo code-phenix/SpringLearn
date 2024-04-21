71f95d543c6df8c43b173a449300b61fc1b51849
codepheonix <55018311+code-phenix@users.noreply.github.com>
2024-04-21 17:39:08 +0530

    first commit

---
+++ src/main/java/com/springLearning/SpringLearning/SpringLearningApplication.java
@@ -0,0 +1,14 @@
+package com.springLearning.SpringLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningApplication.class, args);
+		System.out.println("Working");
+	}
+
+}
